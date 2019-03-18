e2c6a1970f98b70b77195e28f1d63853330abc50
johnny chan <cyj0818nl@163.com>
2019-03-18 15:45:26 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
 
 import AddBlog from './components/AddBlog'
 import ShowBlog from './components/ShowBlog'
-import BlogHeader from './components//BlogHeader'
+import BlogHeader from './components/BlogHeader'
 
 export default {
   name: 'App',
